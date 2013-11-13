104d8dd01408fe2370e11e3046d95302965c6d43
DanWBR <DanWBR@users.noreply.github.com>
2013-11-13 15:40:30 -0200

    Update PipeHydraulicProfile.vb
    Fixed Pipe XML data loading.

--- DWSIM/Objects/Classes/Pipe/PipeHydraulicProfile.vb
+++ DWSIM/Objects/Classes/Pipe/PipeHydraulicProfile.vb
@@ -61,7 +61,7 @@ Imports System.Linq
         For Each xel As XElement In (From xel2 As XElement In data Select xel2 Where xel2.Name = "Sections").Elements.ToList
             Dim pr As New PipeSection()
             pr.LoadData(xel.Elements.ToList)
-            m_collection.Add(xel.@IF, pr)
+            m_collection.Add(xel.@ID, pr)
         Next
 
     End Function
