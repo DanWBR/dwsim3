d0e4cdd484fa52a03a8cf1edbb1ddf839c090b00
DanWBR <danielwag@gmail.com>
2015-08-04 20:35:41 -0400

    Bug fix

--- DWSIM/Objects/PropertyPackages/COSMOSAC/COSMOSACPropertyPackage.vb
+++ DWSIM/Objects/PropertyPackages/COSMOSAC/COSMOSACPropertyPackage.vb
@@ -39,19 +39,12 @@ Namespace DWSIM.SimulationObjects.PropertyPackages
 
         Public Sub New(ByVal comode As Boolean)
             MyBase.New(comode)
-            With Me.Parameters
-                .Add("PP_IDEAL_VAPOR_PHASE_FUG", 1)
-            End With
             Me.m_act = New Auxiliary.COSMO_SAC
         End Sub
 
         Public Sub New()
 
             MyBase.New(False)
-
-            With Me.Parameters
-                .Add("PP_IDEAL_VAPOR_PHASE_FUG", 1)
-            End With
 
             Me.m_act = New Auxiliary.COSMO_SAC
             Me.IsConfigurable = True
