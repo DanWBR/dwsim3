f6573e3d23a71f7b26def608fd7b3cb7398e0069
DanWBR <danielwag@gmail.com>
2015-11-16 20:56:28 -0400

    Added missing file

--- DWSIM/Forms/FormTextBox.Designer.vb
+++ DWSIM/Forms/FormTextBox.Designer.vb
@@ -27,26 +27,23 @@ Partial Class FormTextBox
         '
         'TextBox1
         '
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox1.Location = New System.Drawing.Point(0, 0)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ReadOnly = True
-        Me.TextBox1.Size = New System.Drawing.Size(534, 454)
+        Me.TextBox1.Size = New System.Drawing.Size(640, 377)
         Me.TextBox1.TabIndex = 0
         '
         'FormTextBox
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(534, 454)
+        Me.ClientSize = New System.Drawing.Size(640, 377)
         Me.Controls.Add(Me.TextBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
         Me.Name = "FormTextBox"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "DWSIM"
+        Me.Text = "FormTextBox"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
